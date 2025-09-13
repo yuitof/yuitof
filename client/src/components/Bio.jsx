d69f3727e3522967a742e20c32972f6d5b77bc72
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-13 06:00:04 -0700

    Add style to image

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -2,7 +2,7 @@ export default function Bio() {
     return (
         <>
             <h2>📸Profile</h2>
-            <img src="https://aws-node-express-api-project-610040096594.s3.us-east-1.amazonaws.com/profile.jpg" alt="" />
+            <img src="https://aws-node-express-api-project-610040096594.s3.us-east-1.amazonaws.com/profile.jpg" alt="" style={{width: "200px"}}/>
             <h2>🧑‍💻Skills</h2>
             <h3>CGI</h3>
             <ul>
