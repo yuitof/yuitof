590d7d159f78f8732e9cd817681d207ec095ca2d
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-14 22:30:32 -0700

    Apply Tailwind css

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -1,21 +1,36 @@
 export default function Bio() {
     return (
         <>
-            <h2>📸Profile</h2>
-            <img src="https://aws-node-express-api-project-610040096594.s3.us-east-1.amazonaws.com/profile.jpg" alt="" style={{width: "200px"}}/>
-            <h2>🧑‍💻Skills</h2>
-            <h3>CGI</h3>
-            <ul>
-                <li>Adobe After Effects</li>
-                <li>Blender</li>
-            </ul>
-            <h3>Development</h3>
-            <ul>
-                <li>JavaScript</li>
-                <li>Node.js</li>
-                <li>React</li>
-                <li>Ruby on Rails</li>
-            </ul>
+            <div className="relative isolate overflow-hidden bg-white p-10 lg:overflow-visible">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+                <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                <div className="lg:pr-4">
+                    <div className="lg:max-w-lg">
+                    <h1 className="mt-2 text-2xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-3xl">📸Profile</h1>
+                    <p className="mt-6 text-xl/8 text-gray-700">Yuito Fujiwara / Motion Designer / Developer / An undergraduate @keio_st / I’m aspiring to be a CGI generalist and interested in computer science.</p>
+                    <p className="mt-8">Feel free to get in touch with me via the contact form belwo, SNS, or email</p>
+                    </div>
+                </div>
+                </div>
+                <div className="-mt-12 -ml-12 p-20 lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                    <img src="https://aws-node-express-api-project-610040096594.s3.us-east-1.amazonaws.com/profile.jpg" alt="" className="w-3xl rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-228" />
+                </div>
+                <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:-mt-50">
+                <div className="lg:pr-4">
+                    <div className="max-w-xl text-base/7 text-gray-600 lg:max-w-lg">
+                    <ul role="list" className="space-y-8 text-gray-600">
+                        <li className="flex gap-x-3">
+                        <span><strong className="font-semibold text-gray-900">CGI</strong> Adobe After Effects / Blender</span>
+                        </li>
+                        <li className="flex gap-x-3">
+                        <span><strong className="font-semibold text-gray-900">Software development</strong> Ruby on Rails / JavaScript / Node.js / React</span>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
         </>
     );
 }
--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -32,44 +32,58 @@ export default function ContactForm() {
     }
 
     return (
-        <form onSubmit={(e) => {
+        <div className="isolate bg-white">
+        <form className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={(e) => {
             e.preventDefault();
             sendMessage(formData);
             setForm(emptyForm);
         }}>
-            <textarea
-                placeholder="Message"
-                value={formData.message}
-                onChange={e => setForm(prevFormData => ({
-                        ...prevFormData,
-                        message: e.target.value
-                }))}
-            />
-            <textarea
-                placeholder="Email"
-                value={formData.email}
-                onChange={e => setForm(prevFormData => ({
-                        ...prevFormData,
-                        email: e.target.value
-                }))}
-            />
-            <textarea
-                placeholder="First Name"
-                value={formData.firstname}
-                onChange={e => setForm(prevFormData => ({
-                        ...prevFormData,
-                        firstname: e.target.value
-                }))}
-            />
-            <textarea
-                placeholder="Last Name"
-                value={formData.lastname}
-                onChange={e => setForm(prevFormData => ({
-                        ...prevFormData,
-                        lastname: e.target.value
-                }))}
-            />
-            <button type="submit">Send</button>
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+                <label for="first-name" className="block text-sm/6 font-semibold text-gray-900">First name</label>
+                <div className="mt-2.5">
+                <input id="first-name" type="text" name="first-name" autocomplete="given-name" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" value={formData.firstname}
+                            onChange={e => setForm(prevFormData => ({
+                                    ...prevFormData,
+                                    firstname: e.target.value
+                            }))} />
+                </div>
+            </div>
+            <div>
+                <label for="last-name" className="block text-sm/6 font-semibold text-gray-900">Last name</label>
+                <div className="mt-2.5">
+                <input id="last-name" type="text" name="last-name" autocomplete="family-name" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" value={formData.lastname}
+                            onChange={e => setForm(prevFormData => ({
+                                    ...prevFormData,
+                                    lastname: e.target.value
+                            }))} />
+                </div>
+            </div>
+            <div className="sm:col-span-2">
+                <label for="email" className="block text-sm/6 font-semibold text-gray-900">Email</label>
+                <div className="mt-2.5">
+                <input id="email" type="email" name="email" autocomplete="email" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" value={formData.email}
+                            onChange={e => setForm(prevFormData => ({
+                                    ...prevFormData,
+                                    email: e.target.value
+                            }))} />
+                </div>
+            </div>
+            <div className="sm:col-span-2">
+                <label for="message" className="block text-sm/6 font-semibold text-gray-900">Message</label>
+                <div className="mt-2.5">
+                <textarea id="message" name="message" rows="4" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" value={formData.message}
+                        onChange={e => setForm(prevFormData => ({
+                                ...prevFormData,
+                                message: e.target.value
+                        }))} />
+                </div>
+            </div>
+            </div>
+            <div className="mt-10">
+            <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
+            </div>
         </form>
+        </div>
     );
 }
