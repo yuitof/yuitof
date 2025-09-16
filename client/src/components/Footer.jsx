e64e5efdcd2a39cd3107b345ef0392123194937e
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-16 06:16:29 -0700

    Improve the css of alerts

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="flex justify-between bg-stone-200 px-5">
+        <footer className="flex justify-between bg-stone-200 px-5 fixed bottom-0 left-0 right-0 text-xs">
             <span>&copy; All Rights Reservedspan</span>
             <a href="/privacy_policy.html">Privacy Policy</a>
         </footer>
