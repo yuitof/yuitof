95f01b762a579c44af5991c2197d0b1e35f069d6
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-17 23:02:01 -0700

    Change Basic auth to Bearer

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ exports.sendEmail = async data => {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": `Basic ${process.env.API_KEY}`
+                "Authorization": `Bearer ${process.env.API_KEY}`
             },
             body: JSON.stringify(data),
         });
